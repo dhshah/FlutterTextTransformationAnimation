360c1044bb529019e063d0b68f39ca0683cc9b68
Darshan Shah <darshanshah230@gmail.com>
2019-10-20 13:24:08 -0700

    Text Animation Widget

---
+++ test/text_animation_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:text_animation/text_animation.dart';
+
+void main() {
+  group('_TextTransformationTween', () {
+    test('interpolates text correctly', () {
+      TransformTextTween tween = TransformTextTween(
+          begin: "aaaaa",
+          end: "ccccc",
+          alphabetToPosition: Map.fromIterables(
+              "abcdefghijklmnopqrstuvwxyz".split(''),
+              List.generate(26, (i) => i)),
+          positionToAlphabet: Map.fromIterables(List.generate(26, (i) => i),
+              "abcdefghijklmnopqrstuvwxyz".split('')));
+      expect(tween.lerp(0), "aaaaa");
+      expect(tween.lerp(0.5), "bbbbb");
+      expect(tween.lerp(1), "ccccc");
+    });
+
+    test('interpolates larger end string', () {
+      TransformTextTween tween = TransformTextTween(
+          begin: "a",
+          end: "ccccc",
+          alphabetToPosition: Map.fromIterables(
+              "abcdefghijklmnopqrstuvwxyz".split(''),
+              List.generate(26, (i) => i)),
+          positionToAlphabet: Map.fromIterables(List.generate(26, (i) => i),
+              "abcdefghijklmnopqrstuvwxyz".split('')));
+      expect(tween.lerp(0), "a");
+      expect(tween.lerp(0).length, 1);
+      expect(tween.lerp(0.5).length, 3);
+      expect(tween.lerp(1).length, 5);
+      expect(tween.lerp(1), "ccccc");
+    });
+
+    test('interpolates larger start string', () {
+      TransformTextTween tween = TransformTextTween(
+          begin: "ccccc",
+          end: "a",
+          alphabetToPosition: Map.fromIterables(
+              "abcdefghijklmnopqrstuvwxyz".split(''),
+              List.generate(26, (i) => i)),
+          positionToAlphabet: Map.fromIterables(List.generate(26, (i) => i),
+              "abcdefghijklmnopqrstuvwxyz".split('')));
+      expect(tween.lerp(0).length, 5);
+      expect(tween.lerp(0), "ccccc");
+      expect(tween.lerp(0.5).length, 3);
+      expect(tween.lerp(1).length, 1);
+      expect(tween.lerp(1), "a");
+    });
+  });
+}
