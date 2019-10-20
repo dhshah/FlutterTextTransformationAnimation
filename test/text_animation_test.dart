0d15f222715217a9a7f7ec8b8f0922073595f625
Darshan Shah <darshanshah230@gmail.com>
2019-10-20 14:01:30 -0700

    more renaming of the package to text_transformation_animation

--- test/text_animation_test.dart
+++ test/text_animation_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:text_animation/text_transformation_animation.dart';
+import 'package:text_transformation_animation/text_transformation_animation.dart';
 
 void main() {
   group('_TextTransformationTween', () {
